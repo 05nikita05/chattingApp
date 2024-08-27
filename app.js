b7abdab19d32e105bd003ee593ef2c28af94fe42
Nikita dwivedi <nikitadwivedi205@gmail.com>
2024-08-27 21:44:26 +0530

    add packages

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const http = require('http');
 const server = http.createServer(app);
 const socketIo = require('socket.io');
 const io = socketIo(server);
+const {v4:uuidv4}=require('uuid');
 
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
@@ -17,12 +18,28 @@ app.get('/',function(req,res){
 })
 
 let users=[];
+let waiting=[];
+let room;
 io.on("connection",function(socket){
     socket.on("new user",function(newuser){
         const user = {id:socket.id,newuser}
         users.push(user);
 
         io.emit('user-list',users);
+    })
+
+    socket.on('joinRoom',function(newuser){
+        socket.data.user = {id:socket.id,newuser}
+        waiting.push(socket.data.user);
+        if(waiting.length>1){
+            room = uuidv4();
+            waiting.forEach((userSocket) => {
+                userSocket.join(room);
+            });
+        }
+        waiting = [];
+        io.to(room).emit('startChat', room);
+
     })
 
     socket.on('typing', (username) => {
@@ -34,17 +51,19 @@ io.on("connection",function(socket){
     });
 
     socket.on("chat message",function(chatMessage){
-        const user = users.find(user => user.id === socket.id);
+        const user = users.find(user => user.id === socket.id)|| socket.data.user;
         io.emit('chat message', {
             userId: socket.id,
             username: user ? user.newuser : 'Anonymous', 
             message: chatMessage
-        });
+        }); 
     })
 
     socket.on("disconnect", function() {
         users = users.filter(user => user.id !== socket.id);
         io.emit('user-list', users);
+        waiting = waiting.filter(userSocket => userSocket.id !== socket.id);
+
     });
 })
 
